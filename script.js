35d4911581d309fd1ccb784d9319f8cdef4e8d44
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-07-13 21:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+var a = document.getElementById("url");
+     function result(){
+       var b = document.getElementById("name").value;
+       var c = document.getElementById("year").value;
+       a.innerHTML += b+c;
+     }
